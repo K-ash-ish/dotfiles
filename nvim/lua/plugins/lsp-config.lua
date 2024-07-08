4f1dd89d50c6e80065de835dedf5bde868fc50c2
Kashish <kashishsondhiya@gmail.com>
2024-07-08 16:08:49 +0530

    nvim  config files

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
        require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "tsserver"}
+        ensure_installed = {"lua_ls", "tsserver", "html" }
       })
     end
   },
@@ -19,6 +19,7 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
+      lspconfig.html.setup({})
 
 
       vim.keymap.set('n', 'gh', vim.lsp.buf.hover ,{})
